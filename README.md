a7c8771edc0a79855e3c2bc9b6bfe2ea8e4a70d2
Piotr Protas <piotrek@codequest.com>
2014-09-10 16:33:34 +0200

    import collections from storageroom to contentful

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
 Storageroom to Contentful
+=================
+
+## Description
+This script helps user to dump all data from StorageRoom application (one per time) to JSON files and load data to the Contentful platform.
+
+## Installation
+```
+$ bundle install
+```
+
+## Usage
+Note that you need to specify your account id, application API key, access token and organization id in the script.rb file (```ACCOUNT_ID```,```APPLICATION_API_KEY```,```CONTENTFUL_ACCESS_TOKEN```, ```CONTENTFUL_ORGANIZATION_ID```).
+Your access token can be found at [CMA - documentation](https://www.contentful.com/developers/documentation/content-management-api/#getting-started)
+
+Step 1:
+
+Dump all the data from Storageroom to JSON file and make a proposal for Contentful content types.
+You have to manually modify the structure of collection.
+
+Available types of field on Contentful:
+1. 'Symbol'
+2. 'Text'
+3. 'Integer'
+4. 'Number'
+5. 'Date'
+6. 'Boolean'
+7. 'Link'
+8. 'Array'
+9. 'Object'
+10. 'Location'
+
+Each file in ```'lib/data/collections'``` must be changed.
+Value of "input_type" attribute must belongs to list of available field types.
+
+To create a single field of Entry/Asset type, we must change ```input_type``` to ```Asset``` or ```Entry```
+To create a multiple field of Entries/Assets type, we must change ```"link_type"``` to ```Array``` and add an additional parameter:
+```"link_type": "Entry"```  or  ```"link_type": "Asset"```
+
+Example:
+```
+    "name": "Entries",
+    "identifier": "entries",
+    "input_type": "Array",
+    "required": true,
+    "unique": null,
+    "link_type": "Entry"
+```
+Script execution:
+
+```
+$ ruby script.rb
+```
--- lib/script.rb
+++ lib/script.rb
@@ -1,16 +1,68 @@
 require 'contentful/management'
 require 'storage_room'
 require 'pry'
-require 'pp'
+require 'fileutils'
+
+CONTENTFUL_ACCESS_TOKEN = 'e548877d1c317ee58e5710c793bd2d92419149b1e3c50d47755a19a5deadda00'
+CONTENTFUL_ORGANIZATION_ID = '1EQPR5IHrPx94UY4AViTYO'
 
 ACCOUNT_ID = '4d13574cba05613d25000004'
-APPLICATION_API_KEY = 'DZHpRbsJ7VgFXhybKWmT'
+APPLICATION_API_KEY = 'HKqZqeesYzwmB3DC6eeZ'
 
+Contentful::Management::Client.new(CONTENTFUL_ACCESS_TOKEN)
 StorageRoom.authenticate(ACCOUNT_ID, APPLICATION_API_KEY)
 
-collections = StorageRoom::Collection.all
-collections.instance_variable_get("@_attributes")['resources'].each do |collection|
-  json_collection = JSON.parse collection.to_json
-  File.open("collection_#{collection.name.downcase}.json", "w").write(JSON.pretty_generate(json_collection))
+puts "Actions:\n 1. Download all data from StorageRoom to JSON file\n 2. Import data to Contentful."
+user_action = gets.to_i
+
+def create_contentful_space
+  # puts "Write your contentful name of space:"
+  # name_space = gets
+  # @space = Contentful::Management::Space.create(name: name_space, organization_id: CONTENTFUL_ORGANIZATION_ID)
+  @space = Contentful::Management::Space.find('ene4qtp2sh7u')
 end
 
+def dump_all_data_from_storageroom_to_json_files
+  collections = StorageRoom::Collection.all
+  collections.instance_variable_get("@_attributes")['resources'].each do |collection|
+    json_collection = JSON.parse collection.to_json
+    FileUtils.mkdir_p 'data/collections' unless File.directory?('data/collections')
+    File.open("data/collections/collection_#{collection.name.downcase}.json", "w").write(JSON.pretty_generate(json_collection))
+  end
+end
+
+def import_data_to_contentful
+  $dir_target = "#{Dir.pwd}/data/collections"
+  Dir.glob("#{$dir_target}/*json") do |file_path|
+    collection_file = JSON.parse(File.read(file_path))
+    collection_attribute = collection_file['collection']
+    content_type_name = collection_attribute['entry_type']
+    content_type = @space.content_types.create(name: content_type_name)
+    fields = collection_attribute['fields']
+    fields.each do |field|
+      field_type = field['input_type']
+      if field_type == 'Entry' ||field_type == 'Asset'
+        content_type.fields.create(id: field['identifier'], name: field['name'], type: 'Link', link_type: field['input_type'], required: field['required'])
+      elsif field_type == 'Array'
+        content_type.fields.create(id: field['identifier'], name: field['name'], type: 'Array', items: create_array_field(field))
+      else
+        content_type.fields.create(id: field['identifier'], name: field['name'], type: field['input_type'], required: field['required'])
+      end
+    end
+  end
+end
+
+def create_array_field(params)
+  file = Contentful::Management::Field.new
+  file.type = 'Link'
+  file.link_type = params['link_type']
+  file
+end
+
+case user_action
+  when 1
+    dump_all_data_from_storageroom_to_json_files
+  when 2
+    create_contentful_space
+    import_data_to_contentful
+end

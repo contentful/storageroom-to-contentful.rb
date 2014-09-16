66ebf5b0ea0bbbc80ad7a8e019fc239fb7c5eb60
Tomasz Warkocki <warkocz@gmail.com>
2014-09-16 16:14:48 +0200

    fixing tests

--- spec/lib/storage_room_exporter_spec.rb
+++ spec/lib/storage_room_exporter_spec.rb
@@ -3,11 +3,13 @@ require 'storage_room_exporter'
 
 describe StorageRoomExporter do
 
+  before do
+    StorageRoomExporter.any_instance.stub(:credentials).and_return(YAML.load_file('spec/support/credentials_spec.yaml'))
+  end
+
   context 'collections' do
     it 'export_collections' do
       vcr('collection/export_collections') do
-        # credentials = YAML.load_file('spec/support/credentials_spec.yaml')
-        # StorageRoomExporter.stub(:credentials).and_return(credentials)
         collections = subject.export_collections
         expect(collections.count).to eq 4
         expect(collections.first['@type']).to eq 'Collection'
@@ -16,9 +18,6 @@ describe StorageRoomExporter do
 
     it 'get_request ' do
       vcr('collection/get_request') do
-        st_object = StorageRoomExporter.new
-        credentials = YAML.load_file('spec/support/credentials_spec.yaml')
-        st_object.credentials.stub(:yaml_file).and_return(credentials)
         request = subject.send(:get_request, 'collections')
       end
     end
